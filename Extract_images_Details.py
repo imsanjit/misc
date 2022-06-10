c8a1c6d6ab84551d15dd0b7ce8d500649f884c2e
Sanjit <40655088+imsanjit@users.noreply.github.com>
2022-06-10 12:59:48 +0530

    Extract Image details from multiple urls

---
+++ Extract_images_Details.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+csv_file = open('Image_Details.csv', 'w', newline='')
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['URL', 'Image_Details'])
+
+url_in = []
+
+for i, x in enumerate(url_in):
+    print(f'Working on url :{i + 1} : {x}')
+    response = requests.get(x)
+    if response.status_code == 200:
+        try:
+            html_soup = BeautifulSoup(response.text, 'html.parser')
+            cont = html_soup.find("div", {"class":["primary-content-section", "product-content", "bank-prod-page"]})
+            list_img = []
+            for img in cont.findAll('img'):
+                list_img.append(img)
+            csv_writer.writerow([x, list_img])
+        except:
+            pass
+
+
+csv_file.close()
+print('ALL DONE. Download your file....')
